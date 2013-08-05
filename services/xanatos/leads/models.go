b3a17558214ff7c9e2d5d2152e9c070d593e24bb
DewaldV <dewald.viljoen@gmail.com>
2013-08-04 20:26:58 -0400

    Last fix. Added postdata meta-tags

--- services/xanatos/leads/models.go
+++ services/xanatos/leads/models.go
@@ -12,10 +12,10 @@ type Lead struct {
 }
 
 type LeadService struct {
-	gorest.RestService `root:"/" consumes:"application/json" produces:"application/json"`
+	gorest.RestService `root:"/leads" consumes:"application/json" produces:"application/json"`
 
-	getLead 	gorest.EndPoint `method:"GET" 		path:"/leads/{Id:int}" 	output:"Lead"`
-	createLead	gorest.EndPoint `method:"POST"		path:"/leads" 			postdata:"Lead"`
-	updateLead	gorest.EndPoint `method:"PUT"		path:"/leads/{Id:int}"	postdata:"Lead"`
-	deleteLead	gorest.EndPoint	`method:"DELETE"	path:"/leads/{Id:int}"`
+	getLead 	gorest.EndPoint `method:"GET" 		path:"/{Id:int}" 	output:"Lead"`
+	createLead	gorest.EndPoint `method:"POST"		path:"/" 			postdata:"Lead"`
+	updateLead	gorest.EndPoint `method:"PUT"		path:"/{Id:int}"	postdata:"Lead"`
+	deleteLead	gorest.EndPoint	`method:"DELETE"	path:"/{Id:int}"`
 }
